256ebbb7d8c36bc56cbfc95b463b6be590d2446a
Andrei Gankin <gankin89@gmail.com>
2023-06-21 23:40:26 +0400

    Added benchmarks.

---
+++ tests/BeeHive.Benchmarks/Benchmarks.cs
@@ -0,0 +1,112 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BeeHive.Benchmarks;
+
+public class Benchmarks
+{
+    private const int ComputeRunCount = 1000;
+    private const int ComputeSumOfNumbers = 1000000;
+    private const int DegreeOfParallelism = 4;
+
+    private Hive _hive = null!;
+    private HiveComputation<Unit, int> _hiveComputation = null!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _hive = new Hive();
+        _hiveComputation = _hive.AddComputation<Unit, int>(
+            Compute,
+            config => config
+                .MinLiveThreads(DegreeOfParallelism)
+                .MaxParallelExecution(DegreeOfParallelism)
+                .WithMinLoadScheduling());
+    }
+
+    [Benchmark]
+    public void SequentialComputation()
+    {
+        for (var runIdx = 0; runIdx < ComputeRunCount; runIdx++)
+        {
+            var result = Compute(Unit.Value);
+        }
+    }
+
+    [Benchmark]
+    public void PLinqComputation()
+    {
+        var results = Enumerable.Range(0, ComputeRunCount)
+            .AsParallel().WithDegreeOfParallelism(DegreeOfParallelism)
+            .Select(_ => Compute(Unit.Value));
+        foreach (var result in results)
+        {
+        }
+    }
+
+    [Benchmark]
+    public void HiveComputation()
+    {
+        var results = _hiveComputation.GetNewResultCollection().GetConsumingEnumerable();
+
+        for (var runIdx = 0; runIdx < ComputeRunCount; runIdx++)
+        {
+            _hiveComputation.QueueRequest(Unit.Value);
+        }
+
+        var resultCount = 0;
+        foreach (var result in results)
+        {
+            if (++resultCount == ComputeRunCount)
+            {
+                return;
+            }
+        }
+    }
+
+    [Benchmark]
+    public void ThreadedComputation()
+    {
+        var threads = new List<Thread>();
+
+        Thread CreateComputationThread(int computeRunCount) => new Thread(() => {
+                for (var runIdx = 0; runIdx < computeRunCount; runIdx++)
+                {
+                    var result = Compute(Unit.Value);
+                }
+            });
+
+        var computeRunCountPerThread = (int)Math.Ceiling(1.0 * ComputeRunCount / DegreeOfParallelism);
+        for (var parallelIdx = 0; parallelIdx < DegreeOfParallelism - 1; parallelIdx++)
+        {
+            var thread = CreateComputationThread(computeRunCountPerThread);
+            thread.Start();
+            
+            threads.Add(thread);
+        }
+
+        var runCountLeft = ComputeRunCount - computeRunCountPerThread * (DegreeOfParallelism - 1);
+        var lastThread = CreateComputationThread(runCountLeft);
+        lastThread.Start();
+
+        threads.Add(lastThread);
+
+        foreach (var thread in threads)
+            thread.Join();
+    }
+
+    private static int Compute(Unit unit)
+    {
+        var result = 0;
+        for (var number = 1; number <= ComputeSumOfNumbers; number++)
+        {
+            result += number;
+        }
+
+        return result;
+    }
+
+    private struct Unit
+    {
+        public static readonly Unit Value = new Unit();
+    }
+}
---
+++ tests/BeeHive.Benchmarks/Program.cs
@@ -0,0 +1,4 @@
+﻿using BeeHive.Benchmarks;
+using BenchmarkDotNet.Running;
+
+var summary = BenchmarkRunner.Run<Benchmarks>();
