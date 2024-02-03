2d08e55baac6d24c7f90f835f586f1d991d7561a
Andrei Gankin <gankin89@gmail.com>
2024-02-03 11:00:20 +0400

    Added more types of computation functions.

--- src/BeeHive/Building/Hive.Building.cs
+++ src/BeeHive/Building/Hive.Building.cs
@@ -11,13 +11,31 @@ public static class Hive
         return ToCompute(computeAsync);
     }
 
+    public static HiveBuilder<TRequest, TResult> ToCompute<TRequest, TResult>(Func<TRequest, CancellationToken, TResult> computationFunc)
+    {
+        var computeAsync = computationFunc.ToComputeDelegate();
+        return ToCompute(computeAsync);
+    }
+
     public static HiveBuilder<TRequest, TResult> ToCompute<TRequest, TResult>(Func<TRequest, Task<TResult>> computationFunc)
     {
         var computeAsync = computationFunc.ToComputeDelegate();
         return ToCompute(computeAsync);
     }
 
+    public static HiveBuilder<TRequest, TResult> ToCompute<TRequest, TResult>(Func<TRequest, CancellationToken, Task<TResult>> computationFunc)
+    {
+        var computeAsync = computationFunc.ToComputeDelegate();
+        return ToCompute(computeAsync);
+    }
+
     public static HiveBuilder<TRequest, TResult> ToCompute<TRequest, TResult>(Func<TRequest, ValueTask<TResult>> computationFunc)
+    {
+        var computeAsync = computationFunc.ToComputeDelegate();
+        return ToCompute(computeAsync);
+    }
+
+    public static HiveBuilder<TRequest, TResult> ToCompute<TRequest, TResult>(Func<TRequest, CancellationToken, ValueTask<TResult>> computationFunc)
     {
         var computeAsync = computationFunc.ToComputeDelegate();
         return ToCompute(computeAsync);
--- src/BeeHive/Extensions/FuncExtensions.cs
+++ src/BeeHive/Extensions/FuncExtensions.cs
@@ -11,6 +11,15 @@ internal static class FuncExtensions
         };
     }
 
+    public static Compute<TRequest, TResult> ToComputeDelegate<TRequest, TResult>(this Func<TRequest, CancellationToken, TResult> func)
+    {
+        return (request, cancellationToken) =>
+        {
+            var result = func(request, cancellationToken);
+            return ValueTask.FromResult(result);
+        };
+    }
+
     public static Compute<TRequest, TResult> ToComputeDelegate<TRequest, TResult>(this Func<TRequest, Task<TResult>> func)
     {
         return async (request, _) =>
@@ -20,11 +29,29 @@ internal static class FuncExtensions
         };
     }
 
+    public static Compute<TRequest, TResult> ToComputeDelegate<TRequest, TResult>(this Func<TRequest, CancellationToken, Task<TResult>> func)
+    {
+        return async (request, cancellationToken) =>
+        {
+            var result = await func(request, cancellationToken);
+            return await ValueTask.FromResult(result);
+        };
+    }
+
     public static Compute<TRequest, TResult> ToComputeDelegate<TRequest, TResult>(this Func<TRequest, ValueTask<TResult>> func)
     {
         return async (request, _) =>
         {
             var result = await func(request);
+            return await ValueTask.FromResult(result);
+        };
+    }
+
+    public static Compute<TRequest, TResult> ToComputeDelegate<TRequest, TResult>(this Func<TRequest, CancellationToken, ValueTask<TResult>> func)
+    {
+        return async (request, cancellationToken) =>
+        {
+            var result = await func(request, cancellationToken);
             return await ValueTask.FromResult(result);
         };
     }
